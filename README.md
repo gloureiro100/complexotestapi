4f642d41bf013cafb6ff0b0c3ad4c5cb63422e17
gloureiro100 <gloureiro100@gmail.com>
2021-02-03 00:07:39 -0300

    adicionar endpoint da criação de cliente.

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ Segue a primeira parte do desafio para a entrevista, a parte do Backend em Sprin
 
 Algumas configurações que talvez sejam necessárias:
 * Utilizei um banco de Dados Mysql8 local e talvez seja preciso alterar as configurações de DataSource no arquivo src/main/resources/application.properties.
+
+#Rotas
+POST /cities - Body { "name": "string", "provincy": { "name": "string" } } - Cadastrar cidade
+GET /cities - Lista todas as cidades
+POST /cities - Body: { "name": "string" } - Consultar cidade por parte do nome
+POST /cities/byProvincyName - Body: { "name": "string" } - Consultar cidade pelo estado
+
+
+POST /customers - Body { "name": "string", "sex": "Enum(MALE, FEMALE, OTHERS)", "birthDate": "0000-00-00", "age": number, "city": { "id": number } }
+GET /customers/id - pathparam {id} : id do cliente - Consultar cliente pelo Id
+GET /customers/byName - Body: { "name": "string" } - Consultar Cliente pelo nome
+DELETE /customers/id - pathparam {id} : id do cliente - Remover Cliente 
+PUT /customers/id - Body: {"name": "string"} - Alterar o nome do cliente
--- src/main/java/com/gabriel/albuquerque/complexotest/controllers/CustomerController.java
+++ src/main/java/com/gabriel/albuquerque/complexotest/controllers/CustomerController.java
@@ -1,11 +1,14 @@
 package com.gabriel.albuquerque.complexotest.controllers;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,12 +32,25 @@ public class CustomerController {
 		return service.findAll();
 	}
 
+	@PostMapping
+	public ResponseEntity<?> insertCustomer(@RequestBody CustomerDTO dto) throws URISyntaxException {
+		CustomerDTO result = service.insertCustomer(dto);
+
+		if (result != null) {
+			String uriLocation = "http://localhost:8080/customers/" + result.getId();
+			URI uri = new URI(uriLocation);
+			return ResponseEntity.created(uri).build();
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
 	@GetMapping(path = { "/{id}" })
 	public ResponseEntity<CustomerDTO> findById(@PathVariable long id) {
 		CustomerDTO result = service.findById(id);
 
 		return result != null ? ResponseEntity.ok().body(result) : (ResponseEntity.notFound().build());
-	}	
+	}
 
 	@DeleteMapping(path = { "/{id}" })
 	public ResponseEntity<?> delete(@PathVariable("id") long id) {
